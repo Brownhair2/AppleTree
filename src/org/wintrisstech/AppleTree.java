c3b93624aa3163ad1eca0009d204ece4c2a6b3b6
pia.sri.cw@gmail.com <>
2012-01-30 19:24:55 -0800

    Perfect Moving sub and iceberg, but gone needs work.

--- src/org/wintrisstech/AppleTree.java
+++ src/org/wintrisstech/AppleTree.java
@@ -29,8 +29,13 @@ public class AppleTree extends JComponent implements MouseListener, ActionListen
     private Image weather;
     private Image apple;
     private Image basket;
+    private int appleNum = 0;
+    private int[] appleX = new int[5];
+    private int[] appleY = new int[5];
     
     private int weatherX = 530;
+    
+    private boolean[] appleFalling = new boolean[5];
 
     public static void main(String[] args) throws IOException
     {
@@ -42,7 +47,7 @@ public class AppleTree extends JComponent implements MouseListener, ActionListen
         window.setLocationRelativeTo(null);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
-
+        
         Timer t = new Timer(100, game);
         t.start();
     }
@@ -54,6 +59,24 @@ public class AppleTree extends JComponent implements MouseListener, ActionListen
         weather = ImageIO.read(getClass().getResource("Weather.png"));
         apple = ImageIO.read(getClass().getResource("johnny_automatic_apple.png"));
         basket = ImageIO.read(getClass().getResource("johnny_automatic_5_8_bushel_basket.png"));
+        
+        appleX[0] = 100;
+        appleY[0] = 100;
+                
+        appleX[1] = 190;
+        appleY[1] = 70;
+        
+        appleX[2] = 330;
+        appleY[2] = 130;
+        
+        appleX[3] = 200;
+        appleY[3] = 200;
+        
+        appleX[4] = 350;
+        appleY[4] = 250;
+        
+        
+        
     }
 
     @Override
@@ -68,11 +91,42 @@ public class AppleTree extends JComponent implements MouseListener, ActionListen
         g.drawImage(background, 0, 0, 800, 600, null);
         g.drawImage(weather, weatherX, -50, 350, 437 * 350 / 800, null);
         g.drawImage(tree, 40, 30, 712 * 550 / 800, 550, null);
+        for (int i = 0; i < 5; i++)
+        {
+            g.drawImage(apple, appleX[i], appleY[i], 75, 85, this);
+        }
     }
 
     @Override
     public void mouseClicked(MouseEvent me)
     {
+    appleFalling[appleNum]=true;
+    appleNum++;
+    {
+        
+    }
+    //{
+        //appleFalling[0] = true;
+    //}
+    //else
+    //{
+            //if (appleFalling[1] == false)
+            //{
+                //appleFalling[1] = true;
+            //}
+            //else
+            //{
+                    //if (appleFalling[2] == false)
+                    //{
+                        //appleFalling[2] = true;
+                    //}
+                    //else
+                    //{
+                        
+                    //}
+            //}
+    //}
+        
     }
 
     @Override
@@ -100,5 +154,13 @@ public class AppleTree extends JComponent implements MouseListener, ActionListen
     {
         weatherX -= 1;
         repaint();
+        
+        for (int i = 0; i < 5; i++)
+        {
+            if (appleY[i] < 500 && appleFalling[i] == true)
+            {
+            appleY[i] = appleY[i] + 10;
+            }  
+        }
     }
 }
